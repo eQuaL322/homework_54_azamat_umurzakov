7613408282aacf75ac8487e51ddffc5e3d932090
Azamat Umurzakov <equal32296@gmail.com>
2023-02-19 06:21:46 +0600

    - created models Product and Category - modify urls.py in app directory - created urls.py in store directory

--- app/settings.py
+++ app/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    'store'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'store/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -71,17 +70,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homework_54',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -112,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False, verbose_name="Наименование")
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name="Описание")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время добавления")
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False, verbose_name="Цена")
+    image = models.TextField(max_length=1000, blank=False, null=False, verbose_name="Ссылка на изображение")
+
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+
+class Category(models.Model):
+    product = models.ForeignKey(
+        to="store.Product",
+        verbose_name="Категория",
+        null=False,
+        blank=False,
+        related_name="category",
+        on_delete=models.RESTRICT()
+    )
+    title = models.CharField(max_length=200, blank=False, null=False, verbose_name="Наименование")
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.title} - {self.description}"
